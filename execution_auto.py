83e2d3132a1f197b2d496e2329696c9050aa90a7
Yas-DSW <yascim11@gmail.com>
2022-03-04 10:13:04 +0100

    correction des entrée de l'execution

--- execution_auto.py
+++ execution_auto.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 import os
 
-liste = ["Globicephala_melas_DNAZoo", "Tursiop_truncatus_DNAZoo","Megaptera_novaeangliae_DNAZoo","Globicephala_melas_NCBI", "Tursiop_truncatus_NCBI","Megaptera_novaeangliae_NCBI"]
-
+liste = ["Globicephala_melas", "Tursiop_truncatus","Megaptera_novaeangliae",]
+liste_BD = ["DNAZoo", "NCBI"]
 i=0
+j=0
 
-while i< len(liste):
-	#on génére la commande qui sera utilisé pour lancer le snakemake 
-#	commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/" + liste[i] + "_DNAZoo.fasta /media/newvol/yascimkamel/Pipeline/Snakemake/copie/"+ liste[i] +"/" + liste[i] +"_NCBI.fna "
-#	commande = "snakemake --cores 8 uncompress /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/" + liste[i] + "_DNAZoo.fasta /media/newvol/yascimkamel/Pipeline/Snakemake/copie/"+ liste[i] +"/" + liste[i] +"_NCBI.fna"
-#	commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/read/" + liste[i] + "_DNAZoo_read.txt"
-#	commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/read/" + liste[i] + "_all_read.txt"
-	commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/read/" + liste[i] + "_ligne.txt"
+
+while j< len(liste_BD):
+	while i < len (liste):
+		#on génére la commande qui sera utilisé pour lancer le snakemake 
+	#	commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/" + liste[i] + "_DNAZoo.fasta /media/newvol/yascimkamel/Pipeline/Snakemake/copie/"+ liste[i] +"/" + liste[i] +"_NCBI.fna "
+	#	commande = "snakemake --cores 8 uncompress /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/" + liste[i] + "_DNAZoo.fasta /media/newvol/yascimkamel/Pipeline/Snakemake/copie/"+ liste[i] +"/" + liste[i] +"_NCBI.fna"
+	#	commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/read/" + liste[i] + "_DNAZoo_read.txt"
+	#	commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i] +"/read/" + liste[i] + "_all_read.txt"
+		commande ="snakemake --cores 8 /media/newvol/yascimkamel/Pipeline/Snakemake/copie/" + liste[i]+"/read/" + liste[i] +"_" liste[j] + "_ligne.txt"
 	i+=1
 	print (commande)
 	#on lance la commande
